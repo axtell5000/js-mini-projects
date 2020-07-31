a6b10f1a917729e39ee467eaecfd51f98af4c012
Stephen Axtell <axtell.stephen@gmail.com>
2020-07-31 09:53:47 +0200

    changed structure of project, added initial html and Css for the counter project

--- color-flipper/app.js
+++ color-flipper/app.js
@@ -1,11 +1,10 @@
-const colors = ['green', 'red', 'rgba(133, 122, 200)', '#f15025'];
-const btn = document.getElementById('btn');
-const color = document.querySelector('.color');
+const colors = ["green", "red", "rgba(133, 122, 200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
 
-btn.addEventListener('click', () => {
+btn.addEventListener("click", () => {
   // get a random number bewtween 0 and 3
   const randomNum = generateRandomNum();
-  console.log(randomNum);
   document.body.style.backgroundColor = colors[randomNum];
   color.textContent = colors[randomNum];
 });
